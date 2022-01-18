0cd16c5a2675025aca00e66be2c8031d097c2dbc
hx507 <72583014+hx507@users.noreply.github.com>
2022-01-18 00:01:34 -0600

    Add working sycl implementation

--- ca_main.cxx
+++ ca_main.cxx
@@ -12,12 +12,10 @@
 
 namespace sycl = cl::sycl;
 sycl::queue cl_queue(sycl::default_selector {});
-class ca_kernel_class;
 
 typedef sycl::buffer<cell, 1> cl_buffer;
 static cell *canvas1 = (cell*)calloc(BUFFER_SIZE, 1), *canvas2 = (cell*)calloc(BUFFER_SIZE, 1);
-cl_buffer buffer1(canvas1, BUFFER_SIZE), buffer2(canvas2, BUFFER_SIZE);
-cell* host_canvas = (cell*)calloc(BUFFER_SIZE, 1);
+cl_buffer buffer1 { canvas1, CANVAS_SIZE_X*CANVAS_SIZE_Y }, buffer2 { canvas2, CANVAS_SIZE_X*CANVAS_SIZE_Y };
 
 inline size_t idx(ssize_t X, ssize_t Y)
 {
@@ -37,16 +35,6 @@ inline size_t xdi_y(size_t i)
     return i / CANVAS_SIZE_X;
 }
 
-void init()
-{
-    debug_print("Using %d dimensional canvas of size %zux%zu with %lu bit colors\n", NDIM, CANVAS_SIZE_X, CANVAS_SIZE_Y, N_COLOR_BIT);
-    debug_print("Using two buffer each of size %lu mb, or %lu cells\n", BUFFER_SIZE / 1024 / 1024, BUFFER_SIZE / sizeof(cell));
-    std::cout << "Running on "
-              << cl_queue.get_device().get_info<sycl::info::device::name>()
-              << std::endl;
-    debug_print("Initialization done!\n");
-}
-
 inline unsigned int update_cell_bin_2d(const cell& center, const cell& c1, const cell& c2, const cell& c3, const cell& c4, const cell& c5, const cell& c6, const cell& c7, const cell& c8)
 {
     unsigned int sum = c1.x
@@ -57,53 +45,37 @@ inline unsigned int update_cell_bin_2d(const cell& center, const cell& c1, const
         + c6.x
         + c7.x
         + c8.x;
-    // return (center.x && (sum == 2 || sum == 3)) || ((!center.x) && sum == 3);
     return (center.x && (sum == 2 || sum == 3)) || ((!center.x) && sum == 3);
 }
 
-inline void update(cl_buffer origin, cl_buffer dest)
+inline void update(cl_buffer origin, cl_buffer dest, sycl::queue cl_queue)
 {
-#ifdef USE_SYCL
-    cl_queue.submit([&](sycl::handler& cgh) {
-        auto ori_acc = origin.get_access<sycl::access::mode::read>(cgh);
-        auto dst_acc = origin.get_access<sycl::access::mode::discard_write>(cgh);
+    {
+        cl_queue.submit([&](sycl::handler& cgh) {
+            auto ori_acc = origin.get_access<sycl::access::mode::read>(cgh);
+            auto dst_acc = dest.get_access<sycl::access::mode::discard_write>(cgh);
 
-        cgh.parallel_for(sycl::range<1>(BUFFER_SIZE), [=](sycl::id<1> i) {
-            auto x = xdi_x(i), y = xdi_y(i);
-            dst_acc[i].x = update_cell_bin_2d(ori_acc[i],
-                ori_acc[idx(x - 1, y - 1)],
-                ori_acc[idx(x + 1, y + 1)],
-                ori_acc[idx(x, y - 1)],
-                ori_acc[idx(x - 1, y)],
-                ori_acc[idx(x + 1, y)],
-                ori_acc[idx(x, y + 1)],
-                ori_acc[idx(x - 1, y + 1)],
-                ori_acc[idx(x + 1, y - 1)]);
+            cgh.parallel_for(sycl::range<1>(CANVAS_SIZE_X * CANVAS_SIZE_Y), [=](sycl::id<1> i) {
+                 auto x = xdi_x(i), y = xdi_y(i);
+                 dst_acc[i].x = update_cell_bin_2d(ori_acc[i],
+                 ori_acc[idx(x - 1, y - 1)],
+                 ori_acc[idx(x + 1, y + 1)],
+                 ori_acc[idx(x, y - 1)],
+                 ori_acc[idx(x - 1, y)],
+                 ori_acc[idx(x + 1, y)],
+                 ori_acc[idx(x, y + 1)],
+                 ori_acc[idx(x - 1, y + 1)],
+                 ori_acc[idx(x + 1, y - 1)]);
+            });
         });
-    });
-#else
-    for (ssize_t x = 0; x < CANVAS_SIZE_X; x++) {
-        for (ssize_t y = 0; y < CANVAS_SIZE_Y; y++) {
-            size_t i = idx(x, y);
-            dest[i].x = update_cell_bin_2d(origin[i],
-                origin[idx(x - 1, y - 1)],
-                origin[idx(x + 1, y + 1)],
-                origin[idx(x, y - 1)],
-                origin[idx(x - 1, y)],
-                origin[idx(x + 1, y)],
-                origin[idx(x, y + 1)],
-                origin[idx(x - 1, y + 1)],
-                origin[idx(x + 1, y - 1)]);
-        }
     }
-#endif
 }
 
 inline void copy_into_buffer(cell* src, cl_buffer dst_buf)
 {
     auto dst = dst_buf.get_access<sycl::access::mode::discard_write>();
     // memcpy(dst_acc, src, BUFFER_SIZE);
-    for (size_t i = 0; i < BUFFER_SIZE; i++) {
+    for (size_t i = 0; i < CANVAS_SIZE_X * CANVAS_SIZE_Y; i++) {
         dst[i] = src[i];
     }
 }
@@ -111,7 +83,7 @@ inline void copy_from_buffer(cl_buffer src_buf, cell* dst)
 {
     auto src = src_buf.get_access<sycl::access::mode::read>();
     // memcpy(dst, src_acc, BUFFER_SIZE);
-    for (size_t i = 0; i < BUFFER_SIZE; i++) {
+    for (size_t i = 0; i < CANVAS_SIZE_X * CANVAS_SIZE_Y; i++) {
         dst[i] = src[i];
     }
 }
@@ -134,7 +106,13 @@ inline void print_buffer(cl_buffer src_buf)
 int main(int argc, char** argv)
 {
     debug_print("-------------CA Running-----------\n");
-    init();
+    debug_print("Using %d dimensional canvas of size %zux%zu with %lu bit colors\n", NDIM, CANVAS_SIZE_X, CANVAS_SIZE_Y, N_COLOR_BIT);
+    debug_print("Using two buffer each of size %lu mb, or %lu cells\n", BUFFER_SIZE / 1024 / 1024, BUFFER_SIZE / sizeof(cell));
+
+    std::cout << "Running on "
+              << cl_queue.get_device().get_info<sycl::info::device::name>() << std::endl;
+
+    cell* host_canvas = (cell*)calloc(BUFFER_SIZE, 1);
     for (int i = CANVAS_SIZE_X * 3 / 7; i < CANVAS_SIZE_X * 4 / 7; i++) {
         host_canvas[idx(i, i)].x = 1;
         host_canvas[idx(i, i + 1)].x = 1;
@@ -155,12 +133,12 @@ int main(int argc, char** argv)
 #endif
     debug_print("Display is now ready\n");
     for (int i = 0; i < iteration / 2; i++) {
-        update(buffer1, buffer2);
+        update(buffer1, buffer2, cl_queue);
         // copy_from_buffer(host_canvas, buffer2);
         print_buffer(buffer2);
         usleep(delay);
 
-        update(buffer2, buffer1);
+        update(buffer2, buffer1, cl_queue);
         // copy_from_buffer(host_canvas, buffer1);
         print_buffer(buffer1);
         usleep(delay);
