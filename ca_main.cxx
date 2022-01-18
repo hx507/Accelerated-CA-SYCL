018b0d74053af594b00b6c414df3d6a23de09b2e
hx507 <72583014+hx507@users.noreply.github.com>
2022-01-17 20:09:19 -0600

    Add basic cpu implementation

--- ca_main.cxx
+++ ca_main.cxx
@@ -1,22 +1,142 @@
-// A simple program that computes the square root of a number
 #include <cmath>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+#include "include/config.h"
+
+static cell *cuda_buffer1 = NULL, *cuda_buffer2 = NULL;
+cell* host_buffer = NULL;
+
+inline size_t idx(ssize_t X, ssize_t Y)
 {
-    if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    if (Y >= 0 && X >= 0 && ((Y * CANVAS_SIZE_X + X) < (long)(NUM_CELLS - 1))) {
+        return (Y * CANVAS_SIZE_X + X);
+    } else {
+        return NUM_CELLS - 1;
+    }
+}
+//(((int)Y >= 0 && (int)X >= 0 && ((Y * CANVAS_SIZE_X + X) < NUM_CELLS - 1)) ? (Y * CANVAS_SIZE_X + X) : NUM_CELLS - 1)
+inline size_t xdi_x(size_t i)
+{
+    return i % CANVAS_SIZE_X;
+}
+
+inline size_t xdi_y(size_t i)
+{
+    return i / CANVAS_SIZE_X;
+}
+
+void init()
+{
+    debug_print("Using %d dimensional canvas of size %zux%zu with %lu bit colors\n", NDIM, CANVAS_SIZE_X, CANVAS_SIZE_Y, N_COLOR_BIT);
+    debug_print("Using two buffer each of size %lu mb, or %lu cells\n", BUFFER_SIZE / 1024 / 1024, BUFFER_SIZE / sizeof(cell));
+
+    cuda_buffer1 = (cell*)calloc(BUFFER_SIZE, 1);
+    cuda_buffer2 = (cell*)calloc(BUFFER_SIZE, 1);
+
+    if (host_buffer) {
+        free(host_buffer);
+        host_buffer = NULL;
+    }
+    host_buffer = (cell*)calloc(1, BUFFER_SIZE);
+    debug_print("Initialization done!\n");
+}
+
+inline unsigned int update_cell_bin_2d(cell& center, cell& c1, cell& c2, cell& c3, cell& c4, cell& c5, cell& c6, cell& c7, cell& c8)
+{
+    unsigned int sum = c1.x
+        + c2.x
+        + c3.x
+        + c4.x
+        + c5.x
+        + c6.x
+        + c7.x
+        + c8.x;
+    if (sum < 2 || sum > 3) {
+        return 0;
+    } else if ((center.x && (sum == 2 || sum == 3)) || ((!center.x) && sum == 3)) {
         return 1;
     }
-
-    // convert input to double
-    const double inputValue = atof(argv[1]);
-
-    // calculate square root
-    const double outputValue = sqrt(inputValue);
-    std::cout << "The square root of " << inputValue << " is " << outputValue
-              << std::endl;
     return 0;
+}
+
+inline void update(cell* dest, cell* origin)
+{
+    for (ssize_t x = 0; x < CANVAS_SIZE_X; x++) {
+        for (ssize_t y = 0; y < CANVAS_SIZE_Y; y++) {
+            size_t i = idx(x, y);
+            dest[i].x = update_cell_bin_2d(origin[i],
+                origin[idx(x - 1, y - 1)],
+                origin[idx(x + 1, y + 1)],
+                origin[idx(x, y - 1)],
+                origin[idx(x - 1, y)],
+                origin[idx(x + 1, y)],
+                origin[idx(x, y + 1)],
+                origin[idx(x - 1, y + 1)],
+                origin[idx(x + 1, y - 1)]);
+        }
+    }
+}
+
+inline void copy_buffer_to_host(cell* dst, cell* src)
+{
+    memcpy(dst, src, BUFFER_SIZE);
+}
+inline void copy_buffer_to_device(cell* dst, cell* src)
+{
+    memcpy(dst, src, BUFFER_SIZE);
+}
+
+inline void print_buffer(cell* src)
+{
+#ifdef DO_TERM_DISPLAY
+    fprintf(stdout, "---------------------Iteration-------------------------\n");
+    for (ssize_t x = 0; x < CANVAS_SIZE_X; x++) {
+        for (ssize_t y = 0; y < CANVAS_SIZE_Y; y++) {
+            // debug_print("(%zu, %zu) -> %zu\n", x, y, idx(x, y));
+            fprintf(stdout, src[idx(x, y)].x ? " " : "█");
+        }
+        fprintf(stdout, "|\n");
+    }
+#endif
+}
+
+int main(int argc, char** argv)
+{
+    debug_print("-------------CA Running-----------\n");
+    init();
+    for (int i = CANVAS_SIZE_X * 3 / 7; i < CANVAS_SIZE_X * 4 / 7; i++) {
+        host_buffer[idx(i, i)].x = 1;
+        host_buffer[idx(i, i + 1)].x = 1;
+        host_buffer[idx(i, i - 1)].x = 1;
+        host_buffer[idx(i, i - 5)].x = 1;
+        host_buffer[idx(i - 1, i - 6)].x = 1;
+        host_buffer[idx(i, i - 6)].x = 1;
+    }
+    print_buffer(host_buffer);
+    copy_buffer_to_device(cuda_buffer1, host_buffer);
+
+    int iteration = 80000;
+#ifdef DO_TERM_DISPLAY
+    int delay = 30000;
+#else
+    // int delay = 10000;
+    int delay = 0;
+#endif
+    debug_print("Display is now ready\n");
+    for (int i = 0; i < iteration / 2; i++) {
+        update(cuda_buffer2, cuda_buffer1);
+        copy_buffer_to_host(host_buffer, cuda_buffer2);
+        print_buffer(host_buffer);
+        usleep(delay);
+
+        update(cuda_buffer1, cuda_buffer2);
+        copy_buffer_to_host(host_buffer, cuda_buffer1);
+        print_buffer(host_buffer);
+        usleep(delay);
+    }
 }
---
+++ include/config.h
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef CONFIG_H
+
+#define DEBUG
+
+#define DO_TERM_DISPLAY
+
+#define BLOCK_SIZE 512
+
+#ifndef NDIM
+#define NDIM 2
+#endif
+#ifndef N_COLOR_BIT
+#define N_COLOR_BIT (sizeof(float))
+#endif
+#ifndef CANVAS_SIZE_X
+#define CANVAS_SIZE_X 128L
+//#define CANVAS_SIZE_X 500
+//#define CANVAS_SIZE_X 1920
+//#define CANVAS_SIZE_X ((int)(1919 * 1.5))
+//#define CANVAS_SIZE_X (ssize_t)99551
+#endif
+#ifndef CANVAS_SIZE_Y
+#define CANVAS_SIZE_Y 128L
+//#define CANVAS_SIZE_Y 500
+//#define CANVAS_SIZE_Y 1080
+//#define CANVAS_SIZE_Y ((int)(1080 * 1.5))
+//#define CANVAS_SIZE_Y (ssize_t)99343
+#endif
+#ifndef NUM_RULE
+#define NUM_RULE 10
+#endif
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE ((CANVAS_SIZE_X * CANVAS_SIZE_Y + 1) * sizeof(cell))
+#define NUM_CELLS (BUFFER_SIZE / sizeof(cell))
+#endif
+
+#define RATIO_CELL_TO_PIXEL 1
+#define RENDER_WINDOW_WIDTH (CANVAS_SIZE_X * RATIO_CELL_TO_PIXEL)
+#define RENDER_WINDOW_HEIGHT (CANVAS_SIZE_Y * RATIO_CELL_TO_PIXEL)
+#define RENDER_WIDTH 0.95
+
+#ifdef DEBUG
+#define debug_print(...)     \
+    do {                     \
+        printf(__VA_ARGS__); \
+    } while (0)
+#else
+#define debug_print(...) \
+    do {                 \
+    } while (0)
+#endif
+#define COUNT_OF(x) ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
+
+struct cell;
+typedef struct cell {
+    //unsigned int x : N_COLOR_BIT;
+    float x;
+} cell;
+#endif
